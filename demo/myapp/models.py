9f945e217854336e1e6f6a7fdd5a5604624782f7
AndresVelezR <andresvelezr16@gmail.com>
2024-02-06 22:28:24 -0500

    moved away demo directory

--- demo/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
