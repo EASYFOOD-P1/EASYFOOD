459ead0c97d69c858d9bf2ada43212ec43b7b533
jjvasquezg <jjvasquezg@eafit.edu.co>
2024-03-28 15:20:04 -0500

    Modified App files and added a new migration

--- Easyfood/EasyfoodApp/models.py
+++ Easyfood/EasyfoodApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='../static/EasyFood')
+    price = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- Easyfood/EasyfoodApp/views.py
+++ Easyfood/EasyfoodApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def home(request):
+    products = Product.objects.all
+    return render(request, 'home.html', {'products': products})
+
+def products(request):
+    query = request.GET.get('searchProduct')
+    results = None
+
+    if query:
+        results = Product.objects.filter(title__icontains=query)
+    products = Product.objects.all
+    return render(request, 'products.html', {'products': products, 'results': results})
+
+def login(request):
+    return render(request, 'login.html')
