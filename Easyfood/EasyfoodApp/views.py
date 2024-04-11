1305e5dcc480a7072917c3e083f2910e74b334ba
AndresVelezR <andresvelezr16@gmail.com>
2024-04-11 14:04:14 -0500

    Finally got the rec system to work on EASYFOOD PROJECT, w a poor UI though

--- Easyfood/EasyfoodApp/views.py
+++ Easyfood/EasyfoodApp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from .models import Product
+#from product_recommendations_db import get_embedding, cosine_similarity
+import numpy as np
+import json
+from openai import OpenAI 
+from dotenv import load_dotenv, find_dotenv
+import os
+
+_ = load_dotenv('../openAI.env')
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get('openAI_api_key'),
+)
+def get_embedding(text, model="text-embedding-3-small"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model, dimensions=1536).data[0].embedding
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
 
 def home(request):
     products = Product.objects.all()
@@ -16,3 +35,50 @@ def products(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def recommendations(request):
+
+    #bestProd = Product.objects.get(emb="Sample title")
+    prompt = request.GET.get('searchProduct')
+    best_prod = None
+    sim_max = -1
+
+    if prompt:
+        embedding_prompt = get_embedding(prompt)
+        #embedding_prompt_adjusted = [x for elem in embedding_prompt for x in (elem, elem)]
+
+        products = Product.objects.all()
+
+        for product in products:
+            binary_emb = product.emb
+            rec_emb = list(np.frombuffer(binary_emb, dtype=np.float64))
+            
+            similitud = cosine_similarity(embedding_prompt, rec_emb)
+            if similitud > sim_max:
+                sim_max = similitud
+                best_prod = product
+
+
+    return render(request, 'recommendations.html', {'best_prod':best_prod})
+
+"""
+def recommendations(request):
+    # CALCULAR EL EMBEDDING MAYOR DE LA BD COMPARADO CON EL PROMT Y DESPUES SELECCIONAR EL PRODUCTO QUE TENGA ESE EMBEDDING. 
+
+    json_file_path = 'EasyfoodApp/management/commands/db.json'
+
+    with open(json_file_path, 'r') as file:
+        file_content = file.read()
+        products = json.loads(file_content)
+    prompt = request.GET.get('searchProduct')
+    emb = get_embedding(prompt)
+    sim = []
+    for i in range(len(products)):
+        sim.append(cosine_similarity(emb,products[i]['embedding']))
+    sim = np.array(sim)
+    idx = np.argmax(sim)
+    bestChoiceTitle = products[idx]['title']
+    bestChoiceDesc = products[idx]['description']
+
+    return(request, 'recommendations.html', {'resultado':bestChoiceTitle})
+"""
